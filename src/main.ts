c9f4c37679e16caea9a7711c3a0654c2e4e5c6c2
Geovanny Mendoza <me@geovannycode.com>
2026-03-07 12:00:00 -0500

    Add CORS

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // Habilitar CORS para consumo desde frontend (Hilla/Vaadin)
+  app.enableCors();
+
   // Habilitar validación global
   app.useGlobalPipes(
     new ValidationPipe({
